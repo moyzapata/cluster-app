00183ff17cbf1eca26f827c50aa7a70fdbc7507b
moyzapata <moyzapata99@gmail.com>
2022-02-28 11:05:11 -0600

    for git hub 1.9

--- src/ui/becarios/FormBecarios.js
+++ src/ui/becarios/FormBecarios.js
@@ -1,19 +1,14 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { FormControlLabel, FormLabel, Radio, RadioGroup, styled } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
-import PropTypes from 'prop-types';
-import CircularProgress from '@mui/material/CircularProgress';
 import {
     useNavigate,
 } from 'react-router-dom';
